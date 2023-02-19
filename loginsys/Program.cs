c15b729bfc34d117170a522e3314833336f6ae1a
ADMIN <ADMIN@DESKTOP-ODQ82HQ>
2023-02-19 13:59:05 +0530

    Add project files.

---
+++ loginsys/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loginsys
+{
+    internal class Program
+    {
+        static FileStream FStream;
+        static StreamWriter SWriter;
+        static void Main(string[] args)
+        {
+            demo d = new demo();
+            FStream = new FileStream("F:\\employee\\login.txt", FileMode.Append, FileAccess.Write);
+            SWriter = new StreamWriter(FStream);
+            SWriter.WriteLine("Employee");
+            SWriter.WriteLine(d.heading());
+            SWriter.WriteLine(d.Name());
+            SWriter.WriteLine(d.timing());
+            SWriter.Flush();
+            SWriter.Close();
+            FStream.Close();
+
+
+
+        }
+    }
+}
---
+++ loginsys/demo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loginsys
+{
+    
+    internal class demo
+    {
+     
+        static char hour;
+        static string reason;
+
+       
+        
+        public string heading()
+        {
+            string date = DateTime.Now.ToString("dd-MM-yyyy");
+            string timingg = DateTime.Now.ToString("h:mm:ss tt");
+            Console.WriteLine("\n\n\t\t\t\tEmployee LoginSystem\t\t\tDate:" + date + "\n" + "\t\t\t\t\t\t\t\t\t" + DateTime.Now.ToString("h:mm:ss tt"));
+            return timingg;
+        }
+        public String Name()
+        {
+            string name;
+            Console.Write("\n\nEnter Your Name:");
+            name = Console.ReadLine();
+            for (int i = 0; i <= name.Length - 1; i++)
+            {
+                if (name[i] < 65 || name[i] > 123)
+                {
+                    Console.Write("\n\t\t<<<Invalid format!Please Enter your name>>>");
+                    Console.Write("\n\nEnter Your Name:");
+                    name = Console.ReadLine();
+                }
+            }
+            return name;
+        }
+        public String timing()
+        {
+            string times = DateTime.Now.ToString("t");
+           
+            for(int j=0;j<times.Length;j++)
+            {
+                hour = times[1];
+                break;  
+            }
+            int hours = (int)Char.GetNumericValue(hour);
+            if (hours > 10||hours<6)
+            {
+
+                Console.Write("\n\t\t\t(Office Timing:9.00 Am - 6.00 Pm) Please Login Before 10.00 Am");              
+                Console.Write("\n\nKindly Provide the Reason for Delay:");
+                reason = Console.ReadLine();
+                while(reason.Length==0)
+                {
+                    Console.Write("\n\t\t\t<<<Required Information*>>>");
+                    Console.Write("\n\nKindly Provide the Reason for Delay:");
+                    reason = Console.ReadLine();
+                }
+            }
+            Console.Write("\n\n\t\t\tThank You :) Your Data Registered Successfully");
+            return reason;
+            
+        }
+        
+    }
+    }
